d797c0d492f63ccf6782dea63208e8ce74d5839d
Vu Ket Dong <106828064+CypriumCuprum@users.noreply.github.com>
2024-06-03 22:51:57 +0700

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -173,6 +173,6 @@ ITS_test_loader = DataLoader(dataset=RESIDE_Dataset(os.path.join(path, 'SOTS/SOT
 #     dataset=DatasetLMDB(os.path.join(path, 'OTS/OTS.lmdb'), size=crop_size), batch_size=BS,
 #     shuffle=True, pin_memory=True)
 OTS_test_loader = DataLoader(
-    dataset=RESIDE_Dataset(os.path.join(path, 'SOTS/outdoor'), train=False, size='whole img', format='.png'),
+    dataset=RESIDE_Dataset(os.path.join(path, 'SOTS/SOTS/outdoor'), train=False, size='whole img', format='.png'),
     batch_size=1,
     shuffle=False)
